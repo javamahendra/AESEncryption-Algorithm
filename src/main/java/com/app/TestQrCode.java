35be976f996e636ef4d17adaa3c884fa0f0d1b0d
javamahendra <javabymahendra@gmail.com>
2020-11-25 14:09:17 +0530

    Update TestQrCode.java

--- src/main/java/com/app/TestQrCode.java
+++ src/main/java/com/app/TestQrCode.java
@@ -4,23 +4,23 @@ import java.io.UnsupportedEncodingException;
 
 import sun.misc.BASE64Decoder;
 
-public class TestQrCode {
+public class TestJwtToken {
 	
 	public static void main(String[] args) throws UnsupportedEncodingException, Exception {
 		
-		String sign = null;
+		String jwt = null;
 		
 		 try {
-			 BASE64Decoder decoder = new BASE64Decoder();
-		     String[]  splitSignedText = sign.split("\\.");
+                     BASE64Decoder decoder = new BASE64Decoder();
+		     String[]  splitSignedText = jwt.split("\\.");
 		     String decodedSigned =new String(decoder.decodeBuffer(splitSignedText[0]));
 		     decodedSigned = decodedSigned +"\n Content:"+(new String(decoder.decodeBuffer(splitSignedText[1])));
 		     
 		     decodedSigned.replaceAll("\\\"", "\"");
 		     System.out.println("\nDecoded Text:" + decodedSigned);
 		   } catch (Exception ex) {
-		     
-		    }
+		     System.out.println(ex.getMessage());
+		   }
 	}
 	
 
